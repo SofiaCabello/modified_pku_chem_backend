e2a1e881bc36cc96d39ea95d49c054e8a84de70f
Kagurazaka Chisa <sofiacabello666@outlook.com>
2024-03-07 14:40:26 +0800

    添加最近购买记录机制

--- src/main/java/com/example/pku_chem_backend/controller/PurchaseRecordController.java
+++ src/main/java/com/example/pku_chem_backend/controller/PurchaseRecordController.java
@@ -3,12 +3,20 @@ package com.example.pku_chem_backend.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.pku_chem_backend.entity.PurchaseRecord;
+import com.example.pku_chem_backend.mapper.DrugMapper;
 import com.example.pku_chem_backend.mapper.PurchaseRecordMapper;
+import com.example.pku_chem_backend.mapper.UserMapper;
 import com.example.pku_chem_backend.util.Result;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,6 +25,10 @@ import java.util.List;
 public class PurchaseRecordController {
     @Autowired
     private PurchaseRecordMapper purchaseRecordMapper;
+    @Autowired
+    private DrugMapper drugMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @PostMapping("/getRecord")
     public Result getRecord(
@@ -31,4 +43,34 @@ public class PurchaseRecordController {
         return Result.ok(list).total(pageParam.getTotal());
     }
 
+    @GetMapping("/getRecentRecord")
+    public Result getRecentRecord(){
+        List<PurchaseRecord> list = purchaseRecordMapper.getRecentRecord();
+        List<RecordWithExtraData> result = new ArrayList<>();
+        for(PurchaseRecord record: list){
+            RecordWithExtraData recordWithExtraData = new RecordWithExtraData();
+            recordWithExtraData.setId(record.getId());
+            recordWithExtraData.setDrugId(record.getDrugId());
+            recordWithExtraData.setBuyer(record.getBuyer());
+            recordWithExtraData.setProcessor(record.getProcessor());
+            recordWithExtraData.setQuantity(record.getQuantity());
+            recordWithExtraData.setApproveDate(record.getApproveDate());
+            recordWithExtraData.setRequestDate(record.getRequestDate());
+            recordWithExtraData.setBuyerName(userMapper.selectByUsername(record.getBuyer()).getRealName());
+            recordWithExtraData.setProcessorName(userMapper.selectByUsername(record.getProcessor()).getRealName());
+            recordWithExtraData.setDrugName(drugMapper.selectById(record.getDrugId()).getName());
+            result.add(recordWithExtraData);
+        }
+        return Result.ok(result);
+    }
+
+    @Getter
+    @Setter
+    @Data
+    @ToString
+    private class RecordWithExtraData extends PurchaseRecord {
+        private String drugName;
+        private String buyerName;
+        private String processorName;
+    }
 }
--- src/main/java/com/example/pku_chem_backend/mapper/PurchaseRecordMapper.java
+++ src/main/java/com/example/pku_chem_backend/mapper/PurchaseRecordMapper.java
@@ -4,10 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.pku_chem_backend.entity.PurchaseRecord;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PurchaseRecordMapper extends BaseMapper<PurchaseRecord> {
     @Insert("INSERT INTO purchase_record (id, drug_id, buyer, source, processor, approve_date, request_date, quantity) VALUES (#{id}, #{drugId}, #{buyer}, #{source}, #{processor}, #{approveDate}, #{requestDate}, #{quantity})")
     void insertRecord(Integer id, Integer drugId, String buyer, String source, String processor, String approveDate, String requestDate, Integer quantity);
 
+    @Select("SELECT * FROM purchase_record WHERE approve_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND approve_date <= CURDATE()")
+    List<PurchaseRecord> getRecentRecord();
 }
