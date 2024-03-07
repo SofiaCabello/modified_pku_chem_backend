d1ea023e7a2da9667abc6a0fefe46632df370735
Kagurazaka Chisa <sofiacabello666@outlook.com>
2024-03-07 12:53:46 +0800

    词典表增序排列

--- src/main/java/com/example/pku_chem_backend/mapper/DictionaryMapper.java
+++ src/main/java/com/example/pku_chem_backend/mapper/DictionaryMapper.java
@@ -15,7 +15,7 @@ public interface DictionaryMapper extends BaseMapper<Dictionary> {
     @Select("select type, options from dictionary order by type desc")
     List<Map<String, String>> getDictionaryDesc(); // 获取字典表，type降序
 
-    @Select("select options from dictionary where type = #{type}")
+    @Select("select options from dictionary where type = #{type} order by options asc")
     List<String> getOptionsByType(String type); // 根据type获取options
 
     @Insert("insert into dictionary (type, options) values (#{type}, #{options})")
